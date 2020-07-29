d2af03eb64da974f3131265f6a29cc8266cb4b52
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-07-29 10:03:00 +0700

    rename fitbase to firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+const firebaseConfig = firebase.initializeApp({
+    apiKey: "AIzaSyASNSasFnzTDUTdiOIFZAUOeHmWcyTyMeY",
+    authDomain: "todolist-tut-1d9d9.firebaseapp.com",
+    databaseURL: "https://todolist-tut-1d9d9.firebaseio.com",
+    projectId: "todolist-tut-1d9d9",
+    storageBucket: "todolist-tut-1d9d9.appspot.com",
+    messagingSenderId: "1096623279640",
+    appId: "1:1096623279640:web:41ae8175a7ba38386a3d6d"
+});
+
+export {firebaseConfig as firebase};
