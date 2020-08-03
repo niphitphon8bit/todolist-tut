fa8f350cb0ed0fcf31b7e65f539ee3b7b3b90aa7
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-08-03 15:48:29 +0700

    fixed - change custom usestate to usestate in selected-project-context

--- src/context/selected-project-context.js
+++ src/context/selected-project-context.js
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState } from 'react';
 
 export const SelectedProjectContext = createContext();
 export const SelectedProjectProvider = ({ children }) => {
-    const { SelectedProject, setSelectedProject } = useState('INBOX');
+    const [ SelectedProject, setSelectedProject ] = useState('INBOX');
 
     return (
         <SelectedProjectContext.Provider
