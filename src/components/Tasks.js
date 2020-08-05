7323d53d066f6c00e211209b363e9959f7656da4
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-08-05 15:34:23 +0700

    add  - add selected project logic to file Task.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Checkbox } from '../components/Checkbox'
 import { useTasks } from '../hooks';
+import { collatedTasks } from '../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../context'
+
+
 export const Tasks = () => {
-    const { tasks } = useTasks("1");
 
-    // console.log(tasks)
+    const { selectedProject } = useSelectedProjectValue();
+    const { projects } = useProjectsValue();
+    const { tasks } = useTasks(selectedProject);
 
-    let project_name = '';
+    let projectName = '';
+
+    if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+        projectName = getTitle(projects, selectedProject).name;
+        console.log('projectName 1 : ', projectName);
+    }
+
+    if (collatedTasksExist(selectedProject) && selectedProject) {
+        projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+        console.log('projectName 2 : ', projectName);
+    }
+
+    useEffect(() => {
+        document.title = `${projectName}: Todo list`;
+    })
+
     return (
-        <div className="taks" data-testid="tasks">
+        <div className="tasks" data-testid="tasks">
             <h2 data-test="project-name">
-                {project_name}
+                 {projectName}
             </h2>
             <ul className="tasks__list">
                 {tasks.map(task => (
