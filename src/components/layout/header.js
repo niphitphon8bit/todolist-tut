adf2721d9b3d07946a0abdb1acf0f7e1dda1ba34
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-08-06 11:51:37 +0700

    add dark mode to project

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import {FaPizzaSlice} from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaPizzaSlice } from 'react-icons/fa';
 
-export const Header = () =>{
+export const Header = ({
+    darkMode,
+    setDarkMode
+}) => {
     // const x =1;
+    const [shouldShowMain, setShouldShowMain] = useState(false);
+    const [showQuickAddTask, setShowQuickAddTask] = useState(false);
 
     return (
-    <header className="header" data-testid="header">
-        <nav>
-            <div className="logo">
-                <img src="/images/logo.png" alt="Todolist"></img>
-            </div>
-            <div className="settings">
-                <ul>
-                    <li data-testid="quick-add-task-action" className="settings__add">+</li>
-                    <li data-testid="dark-mode-acion" className="settings__darkmode">
-                        <FaPizzaSlice/>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
+        <header className="header" data-testid="header">
+            <nav>
+                <div className="logo">
+                    <img src="/images/logo.png" alt="Todolist"></img>
+                </div>
+                <div className="settings">
+                    <ul>
+                        <li
+                            data-testid="quick-add-task-action"
+                            className="settings__add"
+                        >
+                            +
+                        </li>
+                        <li
+                            data-testid="dark-mode-acion"
+                            className="settings__darkmode"
+                            onClick={() => setDarkMode(!darkMode)}
+                        >
+                            <FaPizzaSlice />
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     );
 };
-    
